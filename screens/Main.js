7eddd15eb1521788d7a4d4a95b5c39401ae5ae7c
h510-cell <58731327+h510-cell@users.noreply.github.com>
2021-05-24 12:03:41 +0530

    Add files via upload

---
+++ screens/Main.js
@@ -0,0 +1,93 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View,SafeAreaView,Platform,Image,TouchableOpacity,ScrollView } from 'react-native';
+
+import {StatusBar} from 'expo-status-bar';
+import * as FaceDetector from 'expo-face-detector';
+import {Camera} from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+
+
+export default class MainScreen extends React.Component{
+constructor(props){
+        super(props)
+        this.state = {
+            hasCameraPermissions : null,
+            faces : []
+        }
+    }
+    componentDidMount(){
+        Permissions.askAsync(Permissions.CAMERA).then(this.onCameraPermission)
+        }
+        onCameraPermission = (status) =>{
+            this.setState({
+                hasCameraPermissions : status.status === "granted"
+            })
+        }
+        onFacesDetected = (faces) => {
+            this.setState({
+                faces : faces
+            })
+        }
+   
+        onFaceDectectionError = (err) => {
+            console.log(err)
+        }
+render(){
+    const {hasCameraPermissions} = this.state;
+    if(hasCameraPermissions == null){
+        return <View/>
+    }
+    if(hasCameraPermissions == false){
+        return(
+            <View style = {styles.container}>
+            <Text> You Have No Excess To The Camera</Text>
+            </View>
+        )
+    }
+return(
+    <View style = {styles.container}>
+    <SafeAreaView style= {styles.droidSafeArea}/>
+    <View style = {styles.headingContainer}>
+    <Text style = {styles.titleText}>Look At Me App</Text>
+    </View>
+    <View style = {styles.cameraStyle}>
+    <Camera style = {{flex : 1}}type = {Camera.Constants.Type.front}
+    faceDetectorSettings = {{
+        mode : FaceDetector.Constants.Mode.fast,
+        detectLandmarks : FaceDetector.Constants.Landmarks.all,
+        runClassifications : FaceDetector.Constants.Classifications.all,
+    }}
+    onFacesDetected = {this.onFacesDetected}
+    onFaceDetectionError = {this.onFaceDetectionError}
+    />
+    </View>
+    <View style = {styles.actionContainer}>
+    
+    </View>
+    </View>
+)
+}
+}
+
+const styles = StyleSheet.create({
+container : {
+    flex: 1
+},
+droidSafeArea : {
+    marginTop : Platform.OS === "android"?StatusBar.currentHeight : 0
+},
+headingContainer : {
+    flex: 0.1,
+    alignItems: 'center',
+    justifyContent : 'center',
+},
+titleText : {
+    fontSize : 29
+},
+cameraStyle : {
+    flex : 0.66
+},
+actionContainer: {
+
+}
+})
